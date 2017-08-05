3d6f265638605570214042d565967aea4c366937
Jani Mustonen <janijohannes@kapsi.fi>
2017-08-05 17:18:09 +0300

    Added lmdb to nix

--- package.nix
+++ package.nix
@@ -1,10 +1,10 @@
-{ pkgs, stdenv, qtbase, qttranslations, mkDerivation, cmake }:
+{ pkgs, stdenv, qtbase, qttranslations, lmdb, mkDerivation, cmake }:
 stdenv.mkDerivation rec {
   version = "0.1.0";
   name = "nheko-${version}";
   src = ./.;
   nativeBuildInputs = [ cmake ];
-  buildInputs = [ qtbase qttranslations ];
+  buildInputs = [ qtbase qttranslations lmdb ];
   installPhase = ''
     mkdir -p $out/bin
     cp nheko $out/bin/nheko
