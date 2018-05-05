8ee4d7ad15775cd42ebca56834e6b6ee8f4b5d11
Anton Karmanov <bergertroll@openmailbox.org>
2018-05-05 17:03:09 +0500

    Initial Russian translate

---
+++ resources/langs/nheko_ru.ts
@@ -0,0 +1,239 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>CreateRoom</name>
+    <message>
+        <location filename="../../src/dialogs/CreateRoom.cc" line="32"/>
+        <source>CANCEL</source>
+        <translation>ОТМЕНА</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/CreateRoom.cc" line="43"/>
+        <source>Name</source>
+        <translation>Название</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/CreateRoom.cc" line="46"/>
+        <source>Topic</source>
+        <translation>Тема</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/CreateRoom.cc" line="49"/>
+        <source>Alias</source>
+        <translation>Псевдоним</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/CreateRoom.cc" line="57"/>
+        <source>Room Visibility</source>
+        <translation>Видимость комнаты</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/CreateRoom.cc" line="66"/>
+        <source>Room Preset</source>
+        <translation>Шаблон комнаты</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/CreateRoom.cc" line="76"/>
+        <source>Direct Chat</source>
+        <translation>Прямой чат</translation>
+    </message>
+</context>
+<context>
+    <name>DateSeparator</name>
+    <message>
+        <location filename="../../src/timeline/TimelineView.cc" line="54"/>
+        <source>Today</source>
+        <translation>Сегодня</translation>
+    </message>
+    <message>
+        <location filename="../../src/timeline/TimelineView.cc" line="56"/>
+        <source>Yesterday</source>
+        <translation>Вчера</translation>
+    </message>
+</context>
+<context>
+    <name>InviteUsers</name>
+    <message>
+        <location filename="../../src/dialogs/InviteUsers.cc" line="36"/>
+        <source>CANCEL</source>
+        <translation>ОТМЕНА</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/InviteUsers.cc" line="47"/>
+        <source>User ID to invite</source>
+        <translation>Идентификатор пользователя для приглашения</translation>
+    </message>
+</context>
+<context>
+    <name>JoinRoom</name>
+    <message>
+        <location filename="../../src/dialogs/JoinRoom.cc" line="30"/>
+        <source>CANCEL</source>
+        <translation>ОТМЕНА</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/JoinRoom.cc" line="41"/>
+        <source>Room ID or alias</source>
+        <translation>Идентификатор или псевдоним комнаты</translation>
+    </message>
+</context>
+<context>
+    <name>LeaveRoom</name>
+    <message>
+        <location filename="../../src/dialogs/LeaveRoom.cc" line="29"/>
+        <source>CANCEL</source>
+        <translation>ОТМЕНА</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/LeaveRoom.cc" line="39"/>
+        <source>Are you sure you want to leave?</source>
+        <translation>Вы действительно желаете выйти?</translation>
+    </message>
+</context>
+<context>
+    <name>Logout</name>
+    <message>
+        <location filename="../../src/dialogs/Logout.cc" line="47"/>
+        <source>CANCEL</source>
+        <translation>ОТМЕНА</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/Logout.cc" line="57"/>
+        <source>Logout. Are you sure?</source>
+        <translation>Выйти из учётной записи. Вы увереныee?</translation>
+    </message>
+</context>
+<context>
+    <name>MemberList</name>
+    <message>
+        <location filename="../../src/dialogs/MemberList.cpp" line="73"/>
+        <source>Room members</source>
+        <translation>Участники комнаты</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/MemberList.cpp" line="83"/>
+        <source>SHOW MORE</source>
+        <translation>ПОКАЗАТЬ ЕЩЁ</translation>
+    </message>
+</context>
+<context>
+    <name>Panel</name>
+    <message>
+        <location filename="../../src/emoji/Panel.cc" line="125"/>
+        <source>Smileys &amp; People</source>
+        <translation>Рожицы и люди</translation>
+    </message>
+    <message>
+        <location filename="../../src/emoji/Panel.cc" line="129"/>
+        <source>Animals &amp; Nature</source>
+        <translation>Животные и природа</translation>
+    </message>
+    <message>
+        <location filename="../../src/emoji/Panel.cc" line="132"/>
+        <source>Food &amp; Drink</source>
+        <translation>Еда и напитки</translation>
+    </message>
+    <message>
+        <location filename="../../src/emoji/Panel.cc" line="135"/>
+        <source>Activity</source>
+        <translation>Деятельность</translation>
+    </message>
+    <message>
+        <location filename="../../src/emoji/Panel.cc" line="139"/>
+        <source>Travel &amp; Places</source>
+        <translation>Места и путешествия</translation>
+    </message>
+    <message>
+        <location filename="../../src/emoji/Panel.cc" line="142"/>
+        <source>Objects</source>
+        <translation>Предметы</translation>
+    </message>
+    <message>
+        <location filename="../../src/emoji/Panel.cc" line="145"/>
+        <source>Symbols</source>
+        <translation>Символы</translation>
+    </message>
+    <message>
+        <location filename="../../src/emoji/Panel.cc" line="148"/>
+        <source>Flags</source>
+        <translation>Флаги</translation>
+    </message>
+</context>
+<context>
+    <name>PreviewUploadOverlay</name>
+    <message>
+        <location filename="../../src/dialogs/PreviewUploadOverlay.cc" line="41"/>
+        <source>Upload</source>
+        <translation>Загрузить</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/PreviewUploadOverlay.cc" line="42"/>
+        <source>Cancel</source>
+        <translation>Отменить</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/PreviewUploadOverlay.cc" line="114"/>
+        <source>Media type: %1
+Media size: %2
+</source>
+        <translation>Тип файла: %1
+Размер файла: %2
+</translation>
+    </message>
+</context>
+<context>
+    <name>ReCaptcha</name>
+    <message>
+        <location filename="../../src/dialogs/ReCaptcha.cpp" line="34"/>
+        <source>CONFIRM</source>
+        <translation>ПОДТВЕРДИТЬ</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/ReCaptcha.cpp" line="37"/>
+        <source>CANCEL</source>
+        <translation>ОТМЕНА</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/ReCaptcha.cpp" line="48"/>
+        <source>Solve the reCAPTCHA and press the confirm button</source>
+        <translation>Решите reCAPTCHA и нажмите кнопку подтверждения</translation>
+    </message>
+</context>
+<context>
+    <name>ReadReceipts</name>
+    <message>
+        <location filename="../../src/dialogs/ReadReceipts.cc" line="98"/>
+        <source>Read receipts</source>
+        <translation>Подтверждать прочтение</translation>
+    </message>
+</context>
+<context>
+    <name>RoomSettings</name>
+    <message>
+        <location filename="../../src/dialogs/RoomSettings.cpp" line="40"/>
+        <source>CANCEL</source>
+        <translation>ОТМЕНА</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/RoomSettings.cpp" line="52"/>
+        <source>Notifications</source>
+        <translation>Уведомления</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/RoomSettings.cpp" line="55"/>
+        <source>Muted</source>
+        <translation>Заглушено</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/RoomSettings.cpp" line="56"/>
+        <source>Mentions only</source>
+        <translation>Только упоминания</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogs/RoomSettings.cpp" line="57"/>
+        <source>All messages</source>
+        <translation>Все сообщения</translation>
+    </message>
+</context>
+</TS>
