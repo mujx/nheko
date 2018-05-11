8381176943c05cb4e8016878e33c76c6afcb3b6b
Santiago Morales <twahm@protonmail.com>
2018-05-11 18:21:18 -0500

    Fix

--- src/timeline/widgets/ImageItem.cc
+++ src/timeline/widgets/ImageItem.cc
@@ -183,7 +183,7 @@ ImageItem::paintEvent(QPaintEvent *event)
         if (image_.isNull()) {
                 QString elidedText = metrics.elidedText(text_, Qt::ElideRight, max_width_ - 10);
 
-                setFixedSize(qMax(max_width_, metrics.width(elidedText)), qMax(max_height_, fontHeight));
+                setFixedSize(metrics.width(elidedText), fontHeight);
 
                 painter.setFont(font);
                 painter.setPen(QPen(QColor(66, 133, 244)));
