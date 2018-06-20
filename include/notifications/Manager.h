92d7390fb2ebf834aa9912f0b543b033c0a8eae6
Max Sandholm <max@sandholm.org>
2018-06-20 16:17:43 +0300

    New notifications (not fully functional)

--- include/notifications/Manager.h
+++ include/notifications/Manager.h
@@ -1,12 +1,60 @@
 #pragma once
 
+#include <QObject>
 #include <QImage>
 #include <QString>
 
-class NotificationsManager
-{
-public:
-        static void postNotification(const QString &room,
-                                     const QString &user,
-                                     const QString &message);
+#if defined(Q_OS_LINUX)
+#include <QtDBus/QDBusArgument>
+#include <QtDBus/QDBusInterface>
+#endif
+
+struct roomEventId {
+        QString roomId;
+        QString eventId;
 };
+
+class NotificationsManager : public QObject
+{
+        Q_OBJECT
+public:
+        NotificationsManager(QObject *parent = nullptr);
+
+        void postNotification(
+                const QString &roomId,
+                const QString &eventId,
+                const QString &roomName,
+                const QString &senderName,
+                const QString &text,
+                const QImage &icon);
+
+signals:
+        void notificationClicked(
+                const QString roomId,
+                const QString eventId);
+
+#if defined(Q_OS_LINUX)
+private:
+        QDBusInterface dbus;
+        uint showNotification(
+                const QString summary,
+                const QString text,
+                const QImage image);
+
+        // notification ID to (room ID, event ID)
+        QMap<uint, roomEventId> notificationIds;
+
+slots:
+        void actionInvoked(
+                uint id,
+                QString action);
+        void notificationClosed(
+                uint id,
+        uint reason);
+#endif
+};
+
+#if defined(Q_OS_LINUX)
+QDBusArgument& operator<<(QDBusArgument& arg, const QImage& image);
+const QDBusArgument& operator>>(const QDBusArgument& arg, QImage&);
+#endif
--- src/notifications/ManagerMac.mm
+++ src/notifications/ManagerMac.mm
@@ -8,13 +8,26 @@
 @end
 
 void
-NotificationsManager::postNotification(const QString &roomName, const QString &userName, const QString &message)
+NotificationsManager::NotificationsManager(QObject *parent): QObject(parent)
+
+void
+NotificationsManager::postNotification(
+                const QString &roomId,
+                const QString &eventId,
+                const QString &roomName,
+                const QString &senderName,
+                const QString &text,
+                const QImage &icon)
 {
+    Q_UNUSED(roomId);
+    Q_UNUSED(eventId);
+    Q_UNUSED(icon);
+
     NSUserNotification * notif = [[NSUserNotification alloc] init];
 
     notif.title           = roomName.toNSString();
-    notif.subtitle        = QString("%1 sent a message").arg(userName).toNSString();
-    notif.informativeText = message.toNSString();
+    notif.subtitle        = QString("%1 sent a message").arg(senderName).toNSString();
+    notif.informativeText = text.toNSString();
     notif.soundName       = NSUserNotificationDefaultSoundName;
 
     [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification: notif];
