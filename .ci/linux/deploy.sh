21f1603d697481bc93f73fec2923ad5ae7c3e21a
Schnuffle <me@schnuffle.de>
2018-06-22 17:38:34 +0200

    Update deploy.sh

--- .ci/linux/deploy.sh
+++ .ci/linux/deploy.sh
@@ -34,10 +34,8 @@ export ARCH=$(uname -m)
 
 source /opt/qt510/bin/qt510-env.sh
 
-LD_LIBRARY_PATH=".deps/usr/lib/:$LD_LIBRARY_PATH"
-PATH=/usr/local/bin:$PATH
-
-export LD_LIBRARY_PATH PATH
+export LD_LIBRARY_PATH=".deps/usr/lib/:${LD_LIBRARY_PATH}"
+export PATH=/usr/local/bin:${PATH}
 
 ./linuxdeployqt*.AppImage ${DIR}/usr/share/applications/*.desktop -bundle-non-qt-libs
 ./linuxdeployqt*.AppImage ${DIR}/usr/share/applications/*.desktop -appimage
